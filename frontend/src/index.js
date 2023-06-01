825c3b5cb476c49d3baf8d8106d3ca78f3e1256f
Cho JeongTaek <cjt3591@gmail.com>
2023-06-01 23:34:39 +0900

    📦️ Chore :  리코일 셋팅

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import RecoilRootComponent from './components/commons/RecoilRoot';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRootComponent>
+      <App />
+    </RecoilRootComponent>
   </React.StrictMode>,
 );
