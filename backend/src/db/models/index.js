a2cb5bf1bf04174a50d0372b77f5e493ddfc3576
siu2388 <swear428@naver.com>
2023-06-03 00:23:37 +0900

    📦️ Chore :  백엔드 개발환경설정

---
+++ backend/src/db/models/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const DB_URL = process.env.MONGODB_URL || 5001;
+("MongoDB 서버 주소가 설정되지 않았습니다.\n./db/index.ts 파일을 확인해 주세요.");
+
+mongoose.connect(DB_URL);
+const db = mongoose.connection;
+
+db.on("connected", () => console.log("아싸 MongoDB연결완료!  " + DB_URL));
+db.on("error", (error) =>
+  console.error("MongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + error)
+);
+
+export {};
