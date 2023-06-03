152dd9c73899404ae42ee65bb7d8fecaf150879b
siu2388 <swear428@naver.com>
2023-06-03 18:41:09 +0900

    ✨ Feat :  몽고디비 연결설정

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,9 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
   res.send('Hello, HOT6');
 });
