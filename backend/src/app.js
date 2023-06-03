0f976af303a4de71aba07c7151b24df50aadc0b8
[AI7] 이연아 <shha0213@gmail.com>
2023-06-03 23:23:57 +0900

    📦️ Chore :  백엔드 개발환경 셋팅

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,15 @@
+import cors from 'cors';
 import express from 'express';
+import { userAuthRouter } from './routers/userRouter.js';
 
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello, HOT6');
 });
+
+app.use(userAuthRouter);
 
 export { app };
