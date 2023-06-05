77d14c05461d958351472541c729cca87f9b0a7b
[AI7] EUNSIL LEE <eunsilling@gmail.com>
2023-06-05 17:19:32 +0900

    ✨ Feat :  create activity folder

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,8 @@ import cors from 'cors';
 import express from 'express';
 import { userAuthRouter } from './routers/userRouter.js';
 import { errorMiddleware } from './middlewares/errorMiddleware.js';
-import { groupRouter } from './routers/groupRouter.js';
+import { activityRouter } from './routers/activityRouter.js';
+import { actCategoryRouter } from './routers/actCategoryRouter.js';
 
 const app = express();
 
@@ -22,7 +23,8 @@ app.get('/getdata', (req, res) => {
   return;
 });
 app.use(userAuthRouter);
-app.use(groupRouter);
+app.use(activityRouter);
+app.use(actCategoryRouter);
 
 app.use(errorMiddleware);
 
