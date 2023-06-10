2b21c2fc2fd63a9d8fa483c3d559bf06a07944eb
[AI7] 이연아 <shha0213@gmail.com>
2023-06-11 03:13:59 +0900

    ✨ Feat :  그룹 활동 조회

--- backend/src/routers/activityRouter.js
+++ backend/src/routers/activityRouter.js
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import { activityService } from '../services/activityService.js';
 import { upload } from '../middlewares/imageUploadMiddleware.js';
 import { loginRequired } from '../middlewares/loginRequired.js';
+import moment from 'moment';
 
 const activityRouter = Router();
 const imgupload = upload.single('proofImage');
 
+// 활동 등록
 activityRouter.post('/activities', loginRequired, imgupload, async (req, res, next) => {
   try {
     const userId = req.currentUserId;
@@ -26,5 +28,23 @@ activityRouter.post('/activities', loginRequired, imgupload, async (req, res, ne
     next(error);
   }
 });
-//활동 관련기능
+
+// 그룹 활동 조회
+activityRouter.get('/activities/:groupId/:usedDate', loginRequired, async (req, res, next) => {
+  try {
+    const groupId = req.params.groupId;
+    const usedDate = req.params.usedDate;
+
+    const date = moment(usedDate, 'YYYY-MM');
+    const parsedYear = date.year();
+    const parsedMonth = date.month() + 1;
+
+    const activityInfo = await activityService.getActivityInfo(groupId, parsedYear, parsedMonth);
+
+    res.status(200).send(activityInfo);
+  } catch (error) {
+    next(error);
+  }
+});
+
 export { activityRouter };
--- backend/src/services/activityService.js
+++ backend/src/services/activityService.js
@@ -13,6 +13,38 @@ class activityService {
     };
     return Activity.create({ newActivity });
   }
+
+  static async getActivityInfo(groupId, year, month) {
+    const activities = await Activity.find({
+      groupId: groupId,
+      state: '승인',
+      usedDate: {
+        $gte: new Date(year, month - 1, 1),
+        $lt: new Date(year, month, 1),
+      },
+    });
+
+    const activityCount = {
+      tumbler: 0,
+      multipleContainers: 0,
+    };
+
+    for (const activity of activities) {
+      const { category } = activity;
+      activityCount[category]++;
+    }
+
+    const response = {
+      activities: activities.map(activity => ({
+        usedDate: activity.usedDate,
+        name: activity.name,
+        category: activity.category,
+      })),
+    };
+
+    return { ...response, ...activityCount };
+  }
+
   // static async addActivity() {}
   // static async removeActivity() {}
 }
